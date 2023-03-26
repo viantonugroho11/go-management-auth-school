42dedddc44e97d6198822451dbea3c662d64270e
Muhammad Novianto Nugroho <muhammad.nugroho@amartha.com>
2023-03-27 01:55:23 +0700

    fixed recovery

--- middlewares/recovery.go
+++ middlewares/recovery.go
@@ -3,23 +3,40 @@ package middlewares
 import (
 	// "go-management-auth-school/response"
 	"log"
-	"net/http"
+	// "net/http"
 	"runtime/debug"
+
+	"github.com/labstack/echo/v4"
 )
 
 
-func Recovery() func(nextHandler http.Handler) http.Handler {
-	return func(nextHandler http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func Recovery() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
 			defer func() {
 				err := recover()
 				if err != nil {
 					log.Fatalf("%s", debug.Stack())
-					// response.RespondError(w, http.StatusInternalServerError, ErrUnknownError)
 					return
 				}
 			}()
-			nextHandler.ServeHTTP(w, r)
-		})
+			return next(c)
+		}
 	}
 }
+
+// func Recovery() func(nextHandler http.Handler) http.Handler {
+// 	return func(nextHandler http.Handler) http.Handler {
+// 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 			defer func() {
+// 				err := recover()
+// 				if err != nil {
+// 					log.Fatalf("%s", debug.Stack())
+// 					response.RespondError(w, http.StatusInternalServerError, ErrUnknownError)
+// 					return
+// 				}
+// 			}()
+// 			nextHandler.ServeHTTP(w, r)
+// 		})
+// 	}
+// }
