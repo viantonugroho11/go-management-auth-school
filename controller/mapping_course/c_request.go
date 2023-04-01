cb4284ad78bf033ba258760adee6d3ed1d6daff5
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-01 21:11:27 +0700

    build service auth student

--- controller/mapping_course/c_request.go
+++ controller/mapping_course/c_request.go
@@ -4,8 +4,8 @@ import "go-management-auth-school/controller"
 
 
 type MappingCourseParams struct {
-	CourseID int `json:"course_id"`
-	TeacherID int `json:"teacher_id"`
-	LessonID int `json:"lesson_id"`
+	ClassID  string `json:"class_id"`
+	TeacherID string `json:"teacher_id"`
+	LessonID string `json:"lesson_id"`
 	controller.DefaultParameter
 }
--- service/auth/s_auth.go
+++ service/auth/s_auth.go
@@ -4,14 +4,17 @@ import (
 	"context"
 	"go-management-auth-school/config"
 	authLoginRequest "go-management-auth-school/controller/auth"
-	authEntity "go-management-auth-school/entity/auth"
-	userController "go-management-auth-school/controller/user"
-	studentServices "go-management-auth-school/controller/student"
 	mappingCourseServices "go-management-auth-school/controller/mapping_course"
 	mapStudent "go-management-auth-school/controller/mapping_student"
+	studentServices "go-management-auth-school/controller/student"
+	userController "go-management-auth-school/controller/user"
+	authEntity "go-management-auth-school/entity/auth"
+	"go-management-auth-school/entity/class"
 	jwthelper "go-management-auth-school/helper/jwt"
 	userRepo "go-management-auth-school/service/user"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -51,11 +54,13 @@ func (service authService) Login(ctx context.Context, parameter *authLoginReques
 		return
 	}
 
+	// compare password
 	err = bcrypt.CompareHashAndPassword([]byte(dataUser.Password), []byte(parameter.Password))
 	if err != nil {
 		return
 	}
 
+	// generate token
 	dataStudent, err := service.studentService.FindOne(ctx, &studentServices.StudentParams{
 		IdentityID: dataUser.IdentifyID,
 	})
@@ -74,15 +79,35 @@ func (service authService) Login(ctx context.Context, parameter *authLoginReques
 		return
 	}
 
-	dataMapCourse, err := service.mapCourseService.GetMappingCourseByStudentID(ctx, dataUser.ID)
+	dataMapCourse, err := service.mapCourseService.FindAll(ctx, &mappingCourseServices.MappingCourseParams{
+		ClassID: dataMapStudent.ClassID,
+	})
+	if err != nil {
+		return
+	}
+	if len(dataMapCourse) == 0 {
+		err = errors.New("Course not found")
+		return
+	}
 
+	jwtClaims := authEntity.JwtCustomClaimsStudent{
+		Username: dataUser.Username,
+		Firstname: dataStudent.FirstName,
+		Lastname: dataStudent.LastName,
+		Indentity: dataUser.IdentifyID,
+		Type: "student",
+		Phone: dataStudent.Phone,
+		Class: class.Class{
+			// ID: dataMapStudent.ClassID,
+		},
+		MappingCourse: dataMapCourse,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * time.Duration(service.config.JwtAuth.JwtExpireTime)).Unix(),
+			Id: dataUser.ID,
+		},
+	}
 
-
-	// service.mapCourseService.GetMappingCourseByStudentID(ctx, dataUser.ID)
-	// service.
-
-
-
-	jwthelper.JwtGenerator(authEntity.JwtCustomClaimsStudent{},service.config.JwtAuth.JwtSecretKey)
+	jwthelper.JwtGenerator(jwtClaims , service.config.JwtAuth.JwtSecretKey)
 	
+	return
 }
