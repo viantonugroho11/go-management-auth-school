83c0972b08e89ccf7b6b50d45b1f9fcd9725f129
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-08 02:27:31 +0700

    add struct and functional service parent

--- service/parent/s_parent.go
+++ service/parent/s_parent.go
@@ -1,1 +1,16 @@
 package parent
+
+
+type ParentRepo interface {
+}
+
+
+type parentService struct {
+	parentRepo ParentRepo
+}
+
+func NewParentService(repo ParentRepo) *parentService {
+	return &parentService{
+		parentRepo: repo,
+	}
+}
