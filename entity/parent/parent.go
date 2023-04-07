24d30227e3d688489807f1bd3839a83b1c8e3a95
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-07 07:49:50 +0700

    fixed entity parent

--- entity/parent/parent.go
+++ entity/parent/parent.go
@@ -1,1 +1,19 @@
 package parent
+
+
+type Parent struct {
+	ID string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+	Type string `json:"type"`
+
+	Gender int `json:"gender"`
+	Phone string `json:"phone"`
+	WorkID int `json:"work_id"`
+	WorkName string `json:"work_name"`
+	Income int `json:"income"`
+	StudentID string `json:"student_id"`
+	Image string `json:"image"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
