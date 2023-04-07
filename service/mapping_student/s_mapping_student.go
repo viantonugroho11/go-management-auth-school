ced1271c69d15058693e7c8ca147c03e321f63d7
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-08 02:27:21 +0700

    add struct and functional service mapping student

--- service/mapping_student/s_mapping_student.go
+++ service/mapping_student/s_mapping_student.go
@@ -1,1 +1,16 @@
 package mapping_student
+
+
+type MpStudentRepo interface {
+}
+
+
+type mpStudentService struct {
+	mpStudentRepo MpStudentRepo
+}
+
+func NewMappingStudentService(repo MpStudentRepo) *mpStudentService {
+	return &mpStudentService{
+		mpStudentRepo: repo,
+	}
+}
