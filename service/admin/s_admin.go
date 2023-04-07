75aee6b898536396a3e31396be7ffc8c3646b733
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-08 02:25:54 +0700

    add struct and functional service admin

--- service/admin/s_admin.go
+++ service/admin/s_admin.go
@@ -1,1 +1,15 @@
 package admin
+
+type AdminRepo interface {
+}
+
+
+type adminService struct {
+	adminRepo AdminRepo
+}
+
+func NewAdminService(repo AdminRepo) *adminService {
+	return &adminService{
+		adminRepo: repo,
+	}
+}
