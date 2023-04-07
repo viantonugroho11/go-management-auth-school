45210e1085e6e57204cdb631cdd8368a5295a4f5
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-08 02:26:36 +0700

    add struct and functional service lesson

--- service/lesson/s_lesson.go
+++ service/lesson/s_lesson.go
@@ -1,1 +1,15 @@
 package lesson
+
+type LessonRepo interface {
+}
+
+
+type lessonService struct {
+	lessonRepo LessonRepo
+}
+
+func NewLessonService(repo LessonRepo) *lessonService {
+	return &lessonService{
+		lessonRepo: repo,
+	}
+}
