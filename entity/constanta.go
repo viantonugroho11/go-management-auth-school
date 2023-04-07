6930629b0b3e7b0a9f8acc04497a77dcf0a0e96c
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-07 08:14:33 +0700

    add newjsonB

--- entity/constanta.go
+++ entity/constanta.go
@@ -1,2 +1,28 @@
 package entity
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"go-management-auth-school/helper/interfacepkg"
+)
+
+type JSONB map[string]interface{}
+
+func NewJSONB(data interface{}) (jsonb JSONB) {
+	interfacepkg.Convert(data, &jsonb)
+	return
+}
+
+func (a JSONB) Value() (driver.Value, error) {
+	return json.Marshal(a)
+}
+
+func (a *JSONB) Scan(value interface{}) error {
+	b, ok := value.([]byte)
+	if !ok {
+		// return errors.New("type assertion to []byte failed")
+		return nil
+	}
+
+	return json.Unmarshal(b, &a)
+}
