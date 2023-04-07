1531903a1023decd1cec907c08546b237cfdb832
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-08 02:27:06 +0700

    add struct and functional service mapping course

--- service/mapping_course/s_mapping_course.go
+++ service/mapping_course/s_mapping_course.go
@@ -1,1 +1,16 @@
 package mapping_course
+
+
+type MpCourseRepo interface {
+}
+
+
+type mpCourseService struct {
+	mpCourseRepo MpCourseRepo
+}
+
+func NewMappingCourseService(repo MpCourseRepo) *mpCourseService {
+	return &mpCourseService{
+		mpCourseRepo: repo,
+	}
+}
