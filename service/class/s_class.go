9e27f9a21c22f90bbf71137de2a7aeac68bbcba4
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-08 02:26:25 +0700

    add struct and functional service class

--- service/class/s_class.go
+++ service/class/s_class.go
@@ -1,1 +1,15 @@
 package class
+
+type ClassRepo interface {
+}
+
+
+type classService struct {
+	classRepo ClassRepo
+}
+
+func NewClassService(repo ClassRepo) *classService {
+	return &classService{
+		classRepo: repo,
+	}
+}
