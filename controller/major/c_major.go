6189edacf722270b9c0d08f6c5bb46e724bbacfa
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-08 02:22:16 +0700

    add initialroutes in controller major

--- controller/major/c_major.go
+++ controller/major/c_major.go
@@ -1,1 +1,21 @@
 package major
+
+import "github.com/labstack/echo/v4"
+
+
+type MajorService interface {
+}
+
+type majorController struct {
+	majorService MajorService
+}
+
+func NewMajorController(majorServices MajorService) majorController {
+	return majorController{
+		majorService: majorServices,
+	}
+}
+
+func (ctrl majorController) InitializeRoutes(userRouter *echo.Group, adminRouter *echo.Group, staticRouter *echo.Group, authRouter *echo.Group) {
+
+}
