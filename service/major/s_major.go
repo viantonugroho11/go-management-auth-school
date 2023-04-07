137db74b460f2f5560f87b2bf8caa03183672e21
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-08 02:26:49 +0700

    add struct and functional service major

--- service/major/s_major.go
+++ service/major/s_major.go
@@ -1,1 +1,15 @@
 package major
+
+type MajorRepo interface {
+}
+
+
+type majorService struct {
+	majorRepo MajorRepo
+}
+
+func NewMajorService(repo MajorRepo) *majorService {
+	return &majorService{
+		majorRepo: repo,
+	}
+}
