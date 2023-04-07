d6e4393cc08a33b99f2b0403a77341a767630199
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-08 02:24:08 +0700

    add struct and functional admin repo

--- repository/admin/r_admin.go
+++ repository/admin/r_admin.go
@@ -1,1 +1,16 @@
 package admin
+
+import "github.com/jmoiron/sqlx"
+
+
+type adminRepo struct {
+	DbMaster *sqlx.DB
+	DbSlave  *sqlx.DB
+}
+
+func NewAdminRepo(dbMaster ,dbSlave *sqlx.DB) *adminRepo {
+	return &adminRepo{
+		DbMaster: dbMaster,
+		DbSlave:  dbSlave,
+	}
+}
