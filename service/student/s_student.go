d6339ad622adc5d0243ca0f664ab5cb3a5cedb2e
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-07 07:51:18 +0700

    build tx for create student

--- service/student/s_student.go
+++ service/student/s_student.go
@@ -5,12 +5,16 @@ import (
 
 	studentRequset "go-management-auth-school/controller/student"
 	studentEntity "go-management-auth-school/entity/student"
+
+	"github.com/jmoiron/sqlx"
 )
 
 
 type StudentRepo interface {
 	SelectAll(ctx context.Context, parameter *studentRequset.StudentParams) (data []studentEntity.Student, err error)
-	// FindOne(ctx context.Context, parameter *studentRequset.StudentParams) (data studentEntity.Student, err error)
+	FindOne(ctx context.Context, parameter *studentRequset.StudentParams) (data studentEntity.Student, err error)
+	Create(ctx context.Context,tx *sqlx.Tx, input *studentEntity.Student) (res string, err error)
+	CreateTx(ctx context.Context) (tx *sqlx.Tx, err error)
 }
 
 type studentService struct {
@@ -23,7 +27,7 @@ func NewStudentService(repo StudentRepo) *studentService {
 	}
 }
 
-func (service studentService)SelectAll(ctx context.Context, parameter *studentRequset.StudentParams) (data []studentEntity.Student, err error) {
+func (service studentService) SelectAll(ctx context.Context, parameter *studentRequset.StudentParams) (data []studentEntity.Student, err error) {
 	// parameter.Offset, parameter.Limit, parameter.Page, parameter.OrderBy, parameter.Sort =
 	// 	service.SetPaginationParameter(parameter.Page, parameter.Limit, studentEntity.MapOrderBy[parameter.OrderBy], parameter.Sort, studentEntity.OrderBy, studentEntity.OrderByString)
 
