86548602f60baf6603676c88e690910f21e53cb2
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-08 02:21:58 +0700

    add initialroutes in controller class

--- controller/class/c_class.go
+++ controller/class/c_class.go
@@ -1,11 +1,9 @@
 package class
 
-
-import (
-)
+import "github.com/labstack/echo/v4"
 
 type ClassService interface {
-	FindOne(id int)
+	// FindOne(id int)
 }
 
 type classController struct {
@@ -16,4 +14,8 @@ func NewClassController(classService ClassService) classController {
 	return classController{
 		classService: classService,
 	}
+}
+
+func (ctrl classController) InitializeRoutes(userRouter *echo.Group, adminRouter *echo.Group, staticRouter *echo.Group, authRouter *echo.Group) {
+
 }
