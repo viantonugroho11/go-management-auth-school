c4770a17a2952160f6bbfa0d15f449591d1b1159
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-08 02:24:19 +0700

    add struct and functional class repo

--- repository/class/r_class.go
+++ repository/class/r_class.go
@@ -1,1 +1,15 @@
 package class
+
+import "github.com/jmoiron/sqlx"
+
+type classRepo struct {
+	DbMaster *sqlx.DB
+	DbSlave  *sqlx.DB
+}
+
+func NewClassRepo(dbMaster ,dbSlave *sqlx.DB) *classRepo {
+	return &classRepo{
+		DbMaster: dbMaster,
+		DbSlave:  dbSlave,
+	}
+}
