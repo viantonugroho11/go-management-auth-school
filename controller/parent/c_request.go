5a59cc8fd7ea45b9fb2e29b346f0068f80b932a7
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-08 11:51:52 +0700

    add parent Request  c_request

--- controller/parent/c_request.go
+++ controller/parent/c_request.go
@@ -1,1 +1,46 @@
 package parent
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator"
+	parentEntity "go-management-auth-school/entity/parent"
+)
+
+
+type ParentRequest struct {
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+	Type string `json:"type"`
+	Gender int `json:"gender"`
+	Phone string `json:"phone"`
+	WorkID int `json:"work_id"`
+	Income int `json:"income"`
+	Image string `json:"image"`
+	StudentID string `json:"student_id"`
+}
+
+func (i *ParentRequest) Validate() error {
+	err := validator.New().Struct(i)
+	if err != nil {
+		for _, er := range err.(validator.ValidationErrors) {
+			return fmt.Errorf("%v %v", er.Field(), er.ActualTag())
+		}
+	}
+	return nil
+}
+
+func (i *ParentRequest) ToEntity() (res *parentEntity.Parent) {
+	res = &parentEntity.Parent{
+		FirstName: i.FirstName,
+		LastName: i.LastName,
+		WorkID: i.WorkID,
+		Gender: i.Gender,
+		Phone: i.Phone,
+		Image: i.Image,
+		Type: i.Type,
+		Income: i.Income,
+		StudentID: i.StudentID,
+	}
+	return
+}
