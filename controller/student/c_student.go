c02e41f02d5e1947dba160bb30a797706a4a97cd
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-08 11:52:28 +0700

    add router student

--- controller/student/c_student.go
+++ controller/student/c_student.go
@@ -18,9 +18,7 @@ type StudentService interface {
 
 	SelectAll(ctx context.Context, parameter *StudentParams) (data []studentEntity.Student, err error)
 	FindOne(ctx context.Context, parameter *StudentParams) (data studentEntity.Student, err error)
-	
-	
-	
+	Create(ctx context.Context, parameter *studentEntity.Student) (err error)
 }
 
 type studentController struct {
@@ -34,7 +32,8 @@ func NewStudentController(studentServices StudentService) studentController {
 }
 
 func (ctrl studentController) InitializeRoutes(userRouter *echo.Group, adminRouter *echo.Group, staticRouter *echo.Group, authRouter *echo.Group) {
-	userRouter.GET("/student", ctrl.GetStudent())
+	userRouter.GET("", ctrl.GetStudent())
+	userRouter.POST("", ctrl.CreateStudent())
 }
 
 func (ctrl studentController) GetStudent() echo.HandlerFunc{
@@ -54,8 +53,34 @@ func (ctrl studentController) GetStudent() echo.HandlerFunc{
 		log.Println(params)
 		data, err := ctrl.studentServices.SelectAll(ctx, params)
 		if err != nil {
+			return response.RespondError(c,http.StatusBadRequest, err)
+		}
+		return response.RespondSuccess(c,http.StatusOK, FromServices(data),nil)
+	}	
+}
+
+func(ctrl studentController) CreateStudent() echo.HandlerFunc{
+	return func(c echo.Context) error {
+		ctx := c.Request().Context()
+		if ctx == nil {
+			ctx = context.Background()
+		}
+		
+		reqStudent := new(StudentRequest)
+		if err := c.Bind(reqStudent); err != nil {
 			return err
 		}
-		return response.RespondSuccess(c,http.StatusAlreadyReported, FromServices(data),nil)
+
+		if err := reqStudent.Validate(); err != nil {
+			return response.RespondError(c,http.StatusBadRequest, err)
+		}
+
+		reqData := reqStudent.ToService()
+		log.Println(reqData)
+		err := ctrl.studentServices.Create(ctx, reqData)
+		if err != nil {
+			return err
+		}
+		return response.RespondSuccess(c,http.StatusCreated, nil,nil)
 	}	
-} 
+}
