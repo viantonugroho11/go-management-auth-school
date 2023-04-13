0257e8bf44bd1371cd36455cb198392934da9941
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-14 01:56:43 +0700

    add delete at in user

--- entity/user/user.go
+++ entity/user/user.go
@@ -29,7 +29,7 @@ type User struct {
 	Status bool `json:"status"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
-	DeletedAt string `json:"deleted_at"`
+	DeletedAt sql.NullString `json:"deleted_at"`
 }
 
 var (
