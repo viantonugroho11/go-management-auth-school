4e209e314434ee426081f70193169fced54cabe3
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-16 14:57:35 +0700

    add entity in mapping course

--- entity/mapping_course/mapping_course.go
+++ entity/mapping_course/mapping_course.go
@@ -1,9 +1,17 @@
 package mapping_course
 
+import (
+	classEntity "go-management-auth-school/entity/class"
+	lessonEntity "go-management-auth-school/entity/lesson"
+	teacherEntity "go-management-auth-school/entity/teacher"
+)
 
 type MappingCourse struct {
 	ID int `json:"id"`
-	CourseID int `json:"course_id"`
-	TeacherID int `json:"teacher_id"`
-	LessonID int `json:"lesson_id"`
+	ClassID string `json:"class_id"`
+	TeacherID string `json:"teacher_id"`
+	LessonID string `json:"lesson_id"`
+	Class classEntity.Class `json:"class"`
+	Teacher teacherEntity.Teacher `json:"teacher"`
+	Lesson lessonEntity.Lesson `json:"lesson"`
 }
