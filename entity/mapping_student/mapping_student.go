1d027a1e0069789b8de7147b032c82e13a734ac4
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-16 14:57:45 +0700

    entity mapping student

--- entity/mapping_student/mapping_student.go
+++ entity/mapping_student/mapping_student.go
@@ -1,6 +1,10 @@
 package mappingstudent
 
 import (
+	studentEntity "go-management-auth-school/entity/student"
+	classEntity "go-management-auth-school/entity/class"
+	teacherEntity "go-management-auth-school/entity/teacher"
+	mpCourseEntity "go-management-auth-school/entity/mapping_course"
 )
 
 type MappingStudent struct {
@@ -8,4 +12,16 @@ type MappingStudent struct {
 	Indentity string `json:"indentity"`
 	ClassID string `json:"class_id"`
 	Type string `json:"type"`
+	Student studentEntity.Student `json:"student"`
+	Class classEntity.Class `json:"class"`
+	Teacher teacherEntity.Teacher `json:"teacher"`
+	MpCourse []mpCourseEntity.MappingCourse `json:"mp_course"`
 }
+
+var (
+
+	Table = "mapping_student"
+
+	Column = []string{"def.id", "def.indentity_id", "def.class_id", "def.type"}
+
+)
