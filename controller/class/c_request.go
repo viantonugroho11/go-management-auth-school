ed6271e1edb361cc0bf3c9e0f252ecb579b5aa93
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-17 21:56:37 +0700

    add request class

--- controller/class/c_request.go
+++ controller/class/c_request.go
@@ -1,6 +1,13 @@
 package class
 
-import "go-management-auth-school/controller"
+import (
+	"fmt"
+	"go-management-auth-school/controller"
+
+	"github.com/go-playground/validator"
+
+	classEntity "go-management-auth-school/entity/class"
+)
 
 type ClassParams struct {
 	Name  string `json:"name"`
@@ -8,3 +15,30 @@ type ClassParams struct {
 	Level string `json:"level"`
 	controller.DefaultParameter
 }
+
+type ClassRequest struct {
+	Name  string `json:"name"`
+	Major int    `json:"major"`
+	Level string `json:"level"`
+}
+
+func (i *ClassRequest) Validate() error {
+	err := validator.New().Struct(i)
+	if err != nil {
+		for _, er := range err.(validator.ValidationErrors) {
+			return fmt.Errorf("%v %v", er.Field(), er.ActualTag())
+		}
+	}
+	return nil
+}
+
+func (i *ClassRequest) ToEntity() (res *classEntity.Class) {
+	res = &classEntity.Class{
+		Name:  i.Name,
+		MajorID: i.Major,
+		Level: i.Level,
+	}
+	return
+}
+
+
