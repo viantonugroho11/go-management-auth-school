13a2cd3c19b7952226f757cb06a780ee8489fce0
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-17 21:57:23 +0700

    nambah function

--- repository/mapping_student/r_mapping_student.go
+++ repository/mapping_student/r_mapping_student.go
@@ -1,7 +1,12 @@
 package mapping_student
 
-import "github.com/jmoiron/sqlx"
+import (
+	"context"
+	 "github.com/jmoiron/sqlx"
 
+mapStudentController "go-management-auth-school/controller/mapping_student"
+mapStudentEntity "go-management-auth-school/entity/mapping_student"
+)
 
 type mpStudentRepo struct {
 	DbMaster *sqlx.DB
@@ -13,4 +18,19 @@ func NewMpStudentRepo(dbMaster ,dbSlave *sqlx.DB) *mpStudentRepo {
 		DbMaster: dbMaster,
 		DbSlave:  dbSlave,
 	}
+}
+
+func (repo mpStudentRepo) FindAll(ctx context.Context, params *mapStudentController.MappingStudentParams) (data []mapStudentEntity.MappingStudent, err error) {
+	// build query here
+	return
+}
+
+func (repo mpStudentRepo) SelectAll(ctx context.Context, parameter *mapStudentController.MappingStudentParams) (data []mapStudentEntity.MappingStudent, err error) {
+	// build query here
+	return
+}
+
+func (repo mpStudentRepo) FindOne(ctx context.Context, params *mapStudentController.MappingStudentParams) (data mapStudentEntity.MappingStudent, err error) {
+	// build query here
+	return
 }
--- service/mapping_student/s_mapping_student.go
+++ service/mapping_student/s_mapping_student.go
@@ -1,7 +1,17 @@
 package mapping_student
 
+import (
+
+"context"
+
+mapStudentController "go-management-auth-school/controller/mapping_student"
+mapStudentEntity "go-management-auth-school/entity/mapping_student"
+)
 
 type MpStudentRepo interface {
+	FindAll(ctx context.Context, params *mapStudentController.MappingStudentParams) (data []mapStudentEntity.MappingStudent, err error)
+	SelectAll(ctx context.Context, parameter *mapStudentController.MappingStudentParams) (data []mapStudentEntity.MappingStudent, err error)
+	FindOne(ctx context.Context, params *mapStudentController.MappingStudentParams) (data mapStudentEntity.MappingStudent, err error)
 }
 
 
@@ -13,4 +23,16 @@ func NewMappingStudentService(repo MpStudentRepo) *mpStudentService {
 	return &mpStudentService{
 		mpStudentRepo: repo,
 	}
+}
+
+func (service mpStudentService) FindAll(ctx context.Context, params *mapStudentController.MappingStudentParams) (data []mapStudentEntity.MappingStudent, err error) {
+	return
+}
+
+func (service mpStudentService) SelectAll(ctx context.Context, parameter *mapStudentController.MappingStudentParams) (data []mapStudentEntity.MappingStudent, err error) {
+	return
+}
+
+func (service mpStudentService) FindOne(ctx context.Context, params *mapStudentController.MappingStudentParams) (data mapStudentEntity.MappingStudent, err error) {
+	return
 }
