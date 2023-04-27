f1335c9cf58dccd9490af0f37ed05722ba05f114
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-27 13:21:36 +0700

    add interface in parent service

--- service/parent/s_parent.go
+++ service/parent/s_parent.go
@@ -1,7 +1,18 @@
 package parent
 
+import (
+	"context"
+
+
+	parentController "go-management-auth-school/controller/parent"
+	parentEntity "go-management-auth-school/entity/parent"
+
+)
 
 type ParentRepo interface {
+	FindAll(ctx context.Context, params *parentController.ParentParams) (data []parentEntity.Parent, err error)
+	SelectAll(ctx context.Context, parameter *parentController.ParentParams) (data []parentEntity.Parent, err error)
+	FindOne(ctx context.Context, params *parentController.ParentParams) (data parentEntity.Parent, err error)
 }
 
 
@@ -13,4 +24,16 @@ func NewParentService(repo ParentRepo) *parentService {
 	return &parentService{
 		parentRepo: repo,
 	}
+}
+
+func (service parentService) FindAll(ctx context.Context, params *parentController.ParentParams) (data []parentEntity.Parent, err error) {
+	return
+}
+
+func (service parentService) SelectAll(ctx context.Context, parameter *parentController.ParentParams) (data []parentEntity.Parent, err error) {
+	return
+}
+
+func (service parentService) FindOne(ctx context.Context, params *parentController.ParentParams) (data parentEntity.Parent, err error) {
+	return
 }
