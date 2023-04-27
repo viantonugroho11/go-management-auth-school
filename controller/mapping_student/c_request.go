27c7c04d35b8f218ae07e82bf66fdbc5e5af9fdc
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-27 13:17:24 +0700

    add request mapping student

--- controller/mapping_student/c_request.go
+++ controller/mapping_student/c_request.go
@@ -1,10 +1,42 @@
 package mapping_student
 
-import "go-management-auth-school/controller"
+import (
+	"fmt"
+	"go-management-auth-school/controller"
+
+	"github.com/go-playground/validator"
+
+	mapStudentEntity "go-management-auth-school/entity/mapping_student"
+)
 
 type MappingStudentParams struct {
 	Indentity string `json:"indentity"`
 	Type      string `json:"type"`
 	ClassID   string `json:"class_id"`
 	controller.DefaultParameter
+}
+
+type MappingStudentRequest struct {
+	Indentity string `json:"indentity"`
+	Type      string `json:"type"`
+	ClassID   string `json:"class_id"`
+}
+
+func (m *MappingStudentRequest) Validate() error {
+	err := validator.New().Struct(m)
+	if err != nil {
+		for _, er := range err.(validator.ValidationErrors) {
+			return fmt.Errorf("%v %v", er.Field(), er.ActualTag())
+		}
+	}
+	return nil
+}
+
+func (m *MappingStudentRequest) ToEntity() (res *mapStudentEntity.MappingStudent) {
+	res = &mapStudentEntity.MappingStudent{
+		Indentity: m.Indentity,
+		Type:      m.Type,
+		ClassID:   m.ClassID,
+	}
+	return
 }
