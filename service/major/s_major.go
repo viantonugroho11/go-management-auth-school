fa31bf65abc1eea6854b30e7a88fc53f19076210
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-27 13:21:27 +0700

    add interface in major service

--- service/major/s_major.go
+++ service/major/s_major.go
@@ -1,6 +1,15 @@
 package major
 
+import (
+	"context"
+	majorController "go-management-auth-school/controller/major"
+	majorEntity "go-management-auth-school/entity/major"
+)
+
 type MajorRepo interface {
+	FindAll(ctx context.Context, params *majorController.MajorParams) (data []majorEntity.Major, err error)
+	SelectAll(ctx context.Context, parameter *majorController.MajorParams) (data []majorEntity.Major, err error)
+	FindOne(ctx context.Context, params *majorController.MajorParams) (data majorEntity.Major, err error)
 }
 
 
@@ -12,4 +21,16 @@ func NewMajorService(repo MajorRepo) *majorService {
 	return &majorService{
 		majorRepo: repo,
 	}
+}
+
+func (service majorService) FindAll(ctx context.Context, params *majorController.MajorParams) (data []majorEntity.Major, err error) {	
+	return
+}
+
+func (service majorService) SelectAll(ctx context.Context, parameter *majorController.MajorParams) (data []majorEntity.Major, err error) {
+	return
+}
+
+func (service majorService) FindOne(ctx context.Context, params *majorController.MajorParams) (data majorEntity.Major, err error) {
+	return
 }
