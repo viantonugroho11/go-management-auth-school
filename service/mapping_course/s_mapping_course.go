093f28bdea0fd635b9a8c3c35270d381849f469a
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-27 13:21:58 +0700

    add interface in mapping course service

--- service/mapping_course/s_mapping_course.go
+++ service/mapping_course/s_mapping_course.go
@@ -1,7 +1,19 @@
 package mapping_course
 
+import (
+	"context"
+
+	mapCourseController "go-management-auth-school/controller/mapping_course"
+	mapCourseEntity "go-management-auth-school/entity/mapping_course"
+)
+
+
+
 
 type MpCourseRepo interface {
+	FindAll(ctx context.Context, params *mapCourseController.MappingCourseParams) (data []mapCourseEntity.MappingCourse, err error)
+	SelectAll(ctx context.Context, parameter *mapCourseController.MappingCourseParams) (data []mapCourseEntity.MappingCourse, err error)
+	FindOne(ctx context.Context, params *mapCourseController.MappingCourseParams) (data mapCourseEntity.MappingCourse, err error)
 }
 
 
@@ -13,4 +25,19 @@ func NewMappingCourseService(repo MpCourseRepo) *mpCourseService {
 	return &mpCourseService{
 		mpCourseRepo: repo,
 	}
+}
+
+func (service mpCourseService) FindAll(ctx context.Context, params *mapCourseController.MappingCourseParams) (data []mapCourseEntity.MappingCourse, err error) {
+	//logic here
+	return
+}
+
+func (service mpCourseService) SelectAll(ctx context.Context, parameter *mapCourseController.MappingCourseParams) (data []mapCourseEntity.MappingCourse, err error) {
+	//logic here
+	return
+}
+
+func (service mpCourseService) FindOne(ctx context.Context, params *mapCourseController.MappingCourseParams) (data mapCourseEntity.MappingCourse, err error) {
+	//logic here
+	return
 }
