7e413d73915a83978ea79c4cb1042dd6d91efbfa
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-27 13:21:20 +0700

    add interface in lesson service

--- service/lesson/s_lesson.go
+++ service/lesson/s_lesson.go
@@ -1,6 +1,17 @@
 package lesson
 
+import (
+	"context"
+
+	lessonController "go-management-auth-school/controller/lesson"
+	lessonEntity "go-management-auth-school/entity/lesson"
+
+)
+
 type LessonRepo interface {
+	FindAll(ctx context.Context, params *lessonController.LessonParams) (data []lessonEntity.Lesson, err error)
+	SelectAll(ctx context.Context, parameter *lessonController.LessonParams) (data []lessonEntity.Lesson, err error)
+	FindOne(ctx context.Context, params *lessonController.LessonParams) (data lessonEntity.Lesson, err error)
 }
 
 
@@ -12,4 +23,17 @@ func NewLessonService(repo LessonRepo) *lessonService {
 	return &lessonService{
 		lessonRepo: repo,
 	}
+}
+
+
+func (service lessonService) FindAll(ctx context.Context, params *lessonController.LessonParams) (data []lessonEntity.Lesson, err error) {
+	return
+}
+
+func (service lessonService) SelectAll(ctx context.Context, parameter *lessonController.LessonParams) (data []lessonEntity.Lesson, err error) {
+	return
+}
+
+func (service lessonService) FindOne(ctx context.Context, params *lessonController.LessonParams) (data lessonEntity.Lesson, err error) {
+	return
 }
