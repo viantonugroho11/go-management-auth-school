e294f6be32aca05eea632bdce1227ef782c4e724
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-27 13:21:46 +0700

    add interface in teacher service

--- service/teacher/s_teacher.go
+++ service/teacher/s_teacher.go
@@ -1,7 +1,17 @@
 package teacher
 
+import (
+	
+	"context"
+
+	teacherController "go-management-auth-school/controller/teacher"
+	teacherEntity "go-management-auth-school/entity/teacher"
+)
 
 type TeacherRepo interface {
+	FindAll(ctx context.Context, params *teacherController.TeacherParams) (data []teacherEntity.Teacher, err error)
+	SelectAll(ctx context.Context, parameter *teacherController.TeacherParams) (data []teacherEntity.Teacher, err error)
+	FindOne(ctx context.Context, params *teacherController.TeacherParams) (data teacherEntity.Teacher, err error)
 }
 
 
@@ -13,4 +23,16 @@ func NewTeacherService(repo TeacherRepo) *teacherService {
 	return &teacherService{
 		teacherRepo: repo,
 	}
+}
+
+func (service teacherService) FindAll(ctx context.Context, params *teacherController.TeacherParams) (data []teacherEntity.Teacher, err error) {
+	return
+}
+
+func (service teacherService) SelectAll(ctx context.Context, parameter *teacherController.TeacherParams) (data []teacherEntity.Teacher, err error) {
+	return
+}
+
+func (service teacherService) FindOne(ctx context.Context, params *teacherController.TeacherParams) (data teacherEntity.Teacher, err error) {
+	return
 }
