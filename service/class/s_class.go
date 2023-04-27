c51e03c9935c32abd5e3e816443645d3061978a5
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-27 13:20:58 +0700

    add interface in repo

--- service/class/s_class.go
+++ service/class/s_class.go
@@ -1,6 +1,16 @@
 package class
 
+import (
+	"context"
+	classController "go-management-auth-school/controller/class"
+	classEntity "go-management-auth-school/entity/class"
+
+)
+
 type ClassRepo interface {
+	FindAll(ctx context.Context, params *classController.ClassParams) (data []classEntity.Class, err error)
+	SelectAll(ctx context.Context, parameter *classController.ClassParams) (data []classEntity.Class, err error)
+	FindOne(ctx context.Context, params *classController.ClassParams) (data classEntity.Class, err error)
 }
 
 
@@ -12,4 +22,16 @@ func NewClassService(repo ClassRepo) *classService {
 	return &classService{
 		classRepo: repo,
 	}
+}
+
+func (service classService) FindAll(ctx context.Context, params *classController.ClassParams) (data []classEntity.Class, err error) {
+	return
+}
+
+func (service classService) SelectAll(ctx context.Context, parameter *classController.ClassParams) (data []classEntity.Class, err error) {
+	return
+}
+
+func (service classService) FindOne(ctx context.Context, params *classController.ClassParams) (data classEntity.Class, err error) {
+	return
 }
