e9fb73bc63e56e6e2e1a9bd86349ce555463d010
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-04-27 13:17:08 +0700

    add request in mapping course

--- controller/mapping_course/c_request.go
+++ controller/mapping_course/c_request.go
@@ -1,6 +1,12 @@
 package mapping_course
 
-import "go-management-auth-school/controller"
+import (
+	"fmt"
+	"go-management-auth-school/controller"
+
+	"github.com/go-playground/validator"
+	mpCourseEntity "go-management-auth-school/entity/mapping_course"
+)
 
 
 type MappingCourseParams struct {
@@ -8,4 +14,29 @@ type MappingCourseParams struct {
 	TeacherID string `json:"teacher_id"`
 	LessonID string `json:"lesson_id"`
 	controller.DefaultParameter
+}
+
+type MappingCourseRequest struct {
+	ClassID  string `json:"class_id"`
+	TeacherID string `json:"teacher_id"`
+	LessonID string `json:"lesson_id"`
+}
+
+func (m *MappingCourseRequest) Validate() error {
+	err := validator.New().Struct(m)
+	if err != nil {
+		for _, er := range err.(validator.ValidationErrors) {
+			return fmt.Errorf("%v %v", er.Field(), er.ActualTag())
+		}
+	}
+	return nil
+}
+
+func (m *MappingCourseRequest) ToEntity() (res *mpCourseEntity.MappingCourse) {
+	res = &mpCourseEntity.MappingCourse{
+		ClassID:  m.ClassID,
+		TeacherID: m.TeacherID,
+		LessonID: m.LessonID,
+	}
+	return
 }
