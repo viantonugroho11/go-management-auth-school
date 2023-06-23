d36a3840845f0d7debe1e255e893e8209e45e390
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-06-24 04:39:53 +0700

    fixed indentasi

--- controller/auth/c_response.go
+++ controller/auth/c_response.go
@@ -7,49 +7,49 @@ import (
 )
 
 type LoginResponse struct {
-	ExpiredDate string `json:"expired_date"`
-	Token       string `json:"token"`
-	Status 		bool `json:"status"`
+	ExpiredDate         string `json:"expired_date"`
+	Token               string `json:"token"`
+	Status              bool   `json:"status"`
 	ExpiredRefreshToken string `json:"expired_refresh_token"`
-	ResfreshToken string `json:"resfresh_token"`
-	SessionToken string `json:"session_token"`
+	ResfreshToken       string `json:"resfresh_token"`
+	SessionToken        string `json:"session_token"`
 }
 
 type ValidateResponse struct {
-	IdentityID string `json:"identityId"`
-	Username string `json:"username"`
-	Fullname string `json:"fullname"`
-	Email string `json:"email"`
-	Phone string `json:"phone"`
-	Permission string `json:"permission"`
-	IsActive bool `json:"is_active"`
-	ExpiredAt time.Time `json:"expired_at"`
-	RefreshExpiredAt string `json:"refresh_expired_at"`
-	Class string `json:"class"`
-	StudentID string `json:"student_id"`
-	TeacherID string `json:"teacher_id"`
+	IdentityID       string    `json:"identityId"`
+	Username         string    `json:"username"`
+	Fullname         string    `json:"fullname"`
+	Email            string    `json:"email"`
+	Phone            string    `json:"phone"`
+	Permission       string    `json:"permission"`
+	IsActive         bool      `json:"is_active"`
+	ExpiredAt        time.Time `json:"expired_at"`
+	RefreshExpiredAt string    `json:"refresh_expired_at"`
+	Class            string    `json:"class"`
+	StudentID        string    `json:"student_id"`
+	TeacherID        string    `json:"teacher_id"`
 }
 
 func FromServiceLogin(res authEntity.Auth) *LoginResponse {
 	return &LoginResponse{
-		ExpiredDate: res.ExpiredAt,
-		Token:       res.Token,
-		Status:      res.IsActive,
+		ExpiredDate:         res.ExpiredAt,
+		Token:               res.Token,
+		Status:              res.IsActive,
 		ExpiredRefreshToken: res.RefreshExpiredAt,
-		ResfreshToken: res.RefreshToken,
-		SessionToken: res.SessionToken,
+		ResfreshToken:       res.RefreshToken,
+		SessionToken:        res.SessionToken,
 	}
 }
 
 func FromServiceValidate(res authEntity.AuthValidate) *ValidateResponse {
 	return &ValidateResponse{
 		IdentityID: res.Identity,
-		Username: res.User.Username,
-		Fullname: helperStr.GetFullNameStudent(res.Student),
-		Email: res.Student.Email,
-		Phone: res.Student.Phone,
+		Username:   res.User.Username,
+		Fullname:   helperStr.GetFullNameStudent(res.Student),
+		Email:      res.Student.Email,
+		Phone:      res.Student.Phone,
 		Permission: helperStr.IsPermission(res.User.Permission),
-		IsActive: res.User.Status,
-		ExpiredAt: helperStr.ConvertUnixToTime(res.ExpiredAt),
+		IsActive:   res.User.Status,
+		ExpiredAt:  helperStr.ConvertUnixToTime(res.ExpiredAt),
 	}
 }
--- controller/verify_token/c_request.go
+++ controller/verify_token/c_request.go
@@ -8,17 +8,16 @@ import (
 	verifyTokenEntity "go-management-auth-school/entity/verify_token"
 )
 
-
 type VerifyTokenParams struct {
 	Identity string `json:"identity"`
-	Token string `json:"token"`
+	Token    string `json:"token"`
 	controller.DefaultParameter
 }
 
 type VerifyTokenRequest struct {
 	Identity string `json:"identity" validate:"required"`
-	Token string `json:"token" validate:"required"`
-	Expired string `json:"expired" validate:"required"`
+	Token    string `json:"token" validate:"required"`
+	Expired  string `json:"expired" validate:"required"`
 }
 
 func (u *VerifyTokenRequest) Validate() error {
@@ -31,13 +30,11 @@ func (u *VerifyTokenRequest) Validate() error {
 	return nil
 }
 
-//to Service
+// to Service
 func (u *VerifyTokenRequest) ToService() *verifyTokenEntity.VerifyToken {
 	return &verifyTokenEntity.VerifyToken{
-		Identity: u.Identity,
-		Token: u.Token,
+		Identity:  u.Identity,
+		Token:     u.Token,
 		ExpiredAt: u.Expired,
 	}
 }
-
-
